82a6105f88c70c0998e9202d520de8c659de49b6
DESKTOP-I3NV25F\advln <antonio.devega@xarxatecactiva.com>
2024-02-22 12:54:31 +0100

    bbd completada

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="librerias/css/style.css">
-
   <title>Document</title>
 
 </head>
@@ -15,7 +14,6 @@
   include('layout/navbar/navbar.php');
   ?>
   <h1>Bienvenido a Bunkay</h1>
-
 </body>
 
 </html>
