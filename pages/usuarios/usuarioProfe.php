8ef6ba0b413e709a4df4fee2643c6d52e4837833
DESKTOP-I3NV25F\advln <antonio.devega@xarxatecactiva.com>
2024-03-03 09:52:13 +0100

    arreglado usuario diferente no pueda entrar en otro perfil que no sea el suyo

--- pages/usuarios/usuarioProfe.php
+++ pages/usuarios/usuarioProfe.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +10,24 @@
   
 <?php
 session_start();
+
+if (!isset($_SESSION["tipo_usuarios"]) || !isset($_SESSION["id_usuarios"])) {
+  // Si no ha iniciado sesión, redirige a la página de inicio de sesión
+  echo "Acceso denegado. Debes de loguearte.";
+  header("refresh:2;url= areaClientes.php"); // Redirige a la página principal después de 2 segundos
+  exit;
+}
+if ($_SESSION["tipo_usuarios"] == 0) {
+  // Si el usuario es profesor, redirige a la página de profesor
+  echo "no tienes acceso a esa página";
+  header("refresh:2;url= panelDeControl.php");
+  exit;
+} elseif ($_SESSION["tipo_usuarios"] == 2) {
+  // Si el usuario es alumno, redirige a la página
+  echo "Acceso denegado. No tienes permiso para acceder a esta página.";
+  header("refresh:2;url= areaUsuario.php"); // Redirige a la página principal después de 2 segundos
+  exit;
+}
 echo "Bienvenido al panel de Profesores " . $_SESSION['nombre'];
 ?>
 <form action="../../Handlers/logout.php" method="post">
