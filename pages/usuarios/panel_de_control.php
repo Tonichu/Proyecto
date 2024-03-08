2c3ed364e748d8e0fa4e181d120363ba3a7f1125
DESKTOP-I3NV25F\advln <antonio.devega@xarxatecactiva.com>
2024-03-08 10:20:39 +0100

    sesiones

--- pages/usuarios/panel_de_control.php
+++ pages/usuarios/panel_de_control.php
@@ -29,14 +29,57 @@ if(isset($_SESSION["numero_filas"])) {
 $queryUser = "SELECT * FROM usuarios where id_usuarios != $id_usuarios $queryLimit";
 $resultUser = mysqli_query($conexion, $queryUser);
 
+if (!$resultUser) {
+  die("Error en la consulta: " . mysqli_error($conexion));
+}
+
 $queryClases = "SELECT * FROM clases $queryLimit" ;
 $resultClases = mysqli_query($conexion, $queryClases);
+
+if (!$resultClases) {
+  die("Error en la consulta: " . mysqli_error($conexion));
+}
+
 
 $querySalas = "SELECT * FROM salas $queryLimit";
 $resultSalas = mysqli_query($conexion, $querySalas);
 
-$queryMaquinas = "SELECT * FROM maquinas $queryLimit";
+if (!$resultSalas) {
+  die("Error en la consulta: " . mysqli_error($conexion));
+}
+
+
+$queryMaquinas = "SELECT maquinas.id_maquina, maquinas.nombre, maquinas.descripcion, maquinas.fecha_adquisicion, maquinas.ultima_revision, salas.nombre AS nombre_sala
+                  FROM maquinas
+                  INNER JOIN salas ON maquinas.id_sala = salas.id_salas
+                  $queryLimit";
 $resultMaquinas = mysqli_query($conexion, $queryMaquinas);
+
+if (!$resultMaquinas) {
+    die("Error en la consulta: " . mysqli_error($conexion));
+}
+
+$querySesiones = "SELECT sesiones.id, clases.nombre AS nombre_clase, salas.nombre AS nombre_sala, sesiones.fecha_hora 
+                  FROM sesiones 
+                  INNER JOIN clases ON sesiones.id_clases = clases.id_clases 
+                  INNER JOIN salas ON sesiones.id_salas = salas.id_salas $queryLimit";
+$resultSesiones = mysqli_query($conexion, $querySesiones);
+
+if (!$resultSesiones) {
+  die("Error en la consulta: " . mysqli_error($conexion));
+}
+$sqlUserSesiones = "SELECT sesiones.id AS id_sesion, clases.nombre AS nombre_sesion, usuarios.nombre AS nombre_usuario
+        FROM sesiones
+        INNER JOIN clases ON sesiones.id_clases = clases.id_clases
+        INNER JOIN usuarios_sesiones ON sesiones.id = usuarios_sesiones.id_sesion
+        INNER JOIN usuarios ON usuarios_sesiones.id_usuario = usuarios.id_usuarios";
+
+// La consulta funciona ^^
+$resultSesionesUser = mysqli_query($conexion, $sqlUserSesiones);
+
+if (!$resultSesionesUser) {
+    die("Error en la consulta: " . mysqli_error($conexion));
+}
 
 /*
 function obtenerNombreUsuario($id_usuarios, $conexion)
@@ -68,6 +111,18 @@ function obtenerNombreUsuario($id_usuarios, $conexion)
   <input type="number" id="numero_filas" name="numero_filas" min="1" max="10">
   <button type="submit" name="mostrar">Mostrar</button>
 </form>
+<form method="post" action="busqueda.php">
+        <label for="tabla">Tabla:</label>
+        <select name="tabla" id="tabla">
+            <option value="usuarios">Usuarios</option>
+            <option value="clases">Clases</option>
+            <option value="salas">Salas</option>
+            <option value="maquinas">Máquinas</option>
+        </select><br><br>
+        <label for="busqueda">Buscar por nombre en la tabla:</label>
+        <input type="text" id="busqueda" name="busqueda">
+        <input type="submit" value="Buscar">
+    </form>
   <h2>Tabla de Usuarios</h2>
   <a href="formulario_nuevo_user.html"><button>Crear usuario</button></a>
 <table class="tabla">
@@ -178,19 +233,108 @@ function obtenerNombreUsuario($id_usuarios, $conexion)
         <td><?php echo $row['descripcion']; ?></td>
         <td><?php echo $row['fecha_adquisicion']; ?></td>
         <td><?php echo $row['ultima_revision']; ?></td>
-        <td><?php echo $row['id_sala']; ?></td>
+        <td><?php echo $row['nombre_sala']; ?></td>
         <td>
           <!-- Botón para eliminar la maquina -->
           <?php echo "<input type='hidden' name='id' value='" . $row['id_maquina'] . "'>"; ?>
           <a href="../maquinas/eliminar_maquina.php?id=<?php echo $row['id_maquina']; ?>"><button>Eliminar</button></a>
-
           <!-- Botón para modificar la maquina -->
           <a href="../maquinas/modificar_maquina.php?id=<?php echo $row['id_maquina']; ?>"><button>Modificar</button></a>
         </td>
       </tr>
     <?php } ?>
   </table>
+  <h2>Tabla de sesiones</h2>
+  <a href="../sesiones/formulario_nueva_sesion.php"><button>Crear Sesión</button></a>
+  <table class="tabla">
+  <tr>
+      <th>ID</th>
+      <th>Nombre clases</th>
+      <th>Nombre salas</th>
+    </tr>
+    <?php while ($row = mysqli_fetch_assoc($resultSesiones)) { ?>
+      <tr>
+        <td><?php echo $row['id']; ?></td>
+        <td><?php echo $row['nombre_clase']; ?></td>
+        <td><?php echo $row['nombre_sala']; ?></td>
+        <td><?php echo $row['fecha_hora']; ?></td>
+        <td>
+          <!-- Botón para eliminar la sesion -->
+          <?php echo "<input type='hidden' name='id' value='" . $row['id'] . "'>"; ?>
+          <a href="../sesiones/eliminar_sesion.php?id=<?php echo $row['id']; ?>"><button>Eliminar</button></a>
+          <!-- Botón para modificar la sesion -->
+          <a href="../sesiones/modificar_sesion.php?id=<?php echo $row['id']; ?>"><button>Modificar</button></a>
+        </td>
+      </tr>
+    <?php } ?>
+    </table>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <h2>Tabla de user sesiones</h2>
+  <a href="../sesiones/formulario_nueva_sesion.php"><button>Crear Sesión</button></a>
+    <table class="tabla">
+  <tr>
+      <th>Nombre Sesión</th>
+      <th>Nombre Usuario</th>
+    </tr>
+    <?php while ($row = mysqli_fetch_assoc($resultSesiones)) { ?>
+      <tr>
+        <td><?php echo $row['nombre_sesion']; ?></td>
+        <td><?php echo $row['nombre_usuario']; ?></td>
+        <td>
+          <!-- Botón para eliminar la sesión -->
+          <?php echo "<input type='hidden' name='id_sesion' value='" . $row['id_sesion'] . "'>"; ?>
+          <a href="../sesiones/eliminar_sesion.php?id=<?php echo $row['id_sesion']; ?>"><button>Eliminar</button></a>
+          <!-- Botón para modificar la sesión -->
+          <a href="../sesiones/modificar_sesion.php?id=<?php echo $row['id_sesion']; ?>"><button>Modificar</button></a>
+        </td>
+      </tr>
+    <?php } ?>
+</table>
 
 </body>
 
