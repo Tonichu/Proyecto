f7a91e7b4bf2342ea6351890e913c5acc82a69fa
Sazabe <sergiobraa@gmail.com>
2024-03-08 10:18:31 +0100

    calendario

---
+++ pages/calendario/calendario.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendario</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <table class="calendario" >
+        <thead>
+            <tr> 
+                <th>Hora</th>
+                <th>Lunes</th>
+                <th>Martes</th>
+                <th>Miércoles</th>
+                <th>Jueves</th>
+                <th>Viernes</th>
+                <th>Sábado</th>
+                <th>Domingo</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php include 'generar_calendario.php'; ?>
+        </tbody>
+    
+</body>
+</html>
